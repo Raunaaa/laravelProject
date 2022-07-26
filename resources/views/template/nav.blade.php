7731be0612d50e94cca7560ad39a6b24ee206307
AlveyRezaP <rezapahlevialvey@gmail.com>
2022-07-26 11:52:25 +0700

    ini project laravel saya

---
+++ resources/views/template/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav bg-dark">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+    <a class="nav-link" href="#">Home</a>
+    <a class="nav-link" href="#">Produk</a>
+    <a class="nav-link" href="#">Keranjang</a>
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Checkout</a>
+  </nav>
